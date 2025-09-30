cad6edcb2d81ca7a8dae295a3651b4484fa8c97a
Ali İmran Gezgwn <aliimrangzgn23@gmail.com>
2025-09-30 19:42:14 +0300

    Proje dosyası ekle.

---
+++ calculator/calculator.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+float aggregaitonOperator(float number1, float number2);
+float subtractionOperator(float number1, float number2);
+float multiplicationOperator(float number1, float number2);
+float divisionOperator(float number1, float number2);
+
+int main()
+{
+	float number1, number2;
+	char operation;
+	cout << "Enter first number: ";
+	cin >> number1;
+	cout << "Enter second number: ";
+	cin >> number2;
+	cout << "Enter operation (+, -, *, /): ";
+	cin >> operation;
+	switch (operation)
+	{
+	case '+':
+		cout << "Result: " << aggregaitonOperator(number1, number2) << endl;
+		break;
+	case '-':
+		cout << "Result: " << subtractionOperator(number1, number2) << endl;
+		break;
+	case '*':
+		cout << "Result: " << multiplicationOperator(number1, number2) << endl;
+		break;
+	case '/':
+		if (number2 != 0)
+			cout << "Result: " << divisionOperator(number1, number2) << endl;
+		else
+			cout << "Error: Division by zero!" << endl;
+		break;
+	default:
+		cout << "Error: Invalid operation!" << endl;
+		break;
+	}
+	return 0;
+}
+
+float aggregaitonOperator(float number1, float number2)
+{	
+	return number1 + number2;
+}
+
+float subtractionOperator(float number1, float number2)
+{
+	return number1 - number2;
+}
+
+float multiplicationOperator(float number1, float number2)
+{
+	return number1 * number2;	
+}
+
+float divisionOperator(float number1, float number2)
+{
+	return number1 / number2;
+}
